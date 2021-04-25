a176a83fcf230818c56fbe9d94ee1018b4567571
Brook Beyene <brookbeyene37@gmail.com>
2021-04-25 12:47:29 -0600

    added py version rotate array from leetcode

--- python_version/main.py
+++ python_version/main.py
@@ -1,13 +1,27 @@
+# def removeDuplicates(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+        # N = len(nums)
+        # if N <= 1: return N
+        # i = j = 1
+        # while i < N:
+        #     if nums[i] != nums[i-1]:
+        #         nums[j] = nums[i]
+        #         j +=1
+        #     i +=1
+        # return j
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     removeDuplicates([1,2,2,3,4,5,5,5])
 
 
-
-def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class RemoveDuplicates:
+    def removeDuplicates(self, nums):
         N = len(nums)
-        if N <= 1: return N
+        if N <= 1: 
+            return N
         i = j = 1
         while i < N:
             if nums[i] != nums[i-1]:
@@ -15,14 +29,10 @@ def removeDuplicates(self, nums):
                 j +=1
             i +=1
         return j
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-<<<<<<< HEAD
-    name = input("hello? ")
-    print('PyCharm' + name)
-=======
-   removeDuplicates([1,2,2,3,4,5,5,5])
-     
->>>>>>> 412fb97617c68c723a774c67d70bdc4634de547a
+test_Remove = RemoveDuplicates()
+print(test_Remove.removeDuplicates([1,2,2,3,4,5,5,5]))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+     
+
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
